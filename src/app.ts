19ace1a443f46fefb31492dc384fa20b31cdfcc1
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-03-20 01:19:51 +0530

    setting cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-const expenseRoutes = require ('./routes/expenseRoutes');
-const userRoutes = require ('./routes/userRoutes');
+const expenseRoutes = require('./routes/expenseRoutes');
+const userRoutes = require('./routes/userRoutes');
 const incomeRoutes = require('./routes/incomeRoutes');
 const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://finsnap.vercel.app/"
+}
+));
 
 app.get('/health', (req, res) => {
     res.status(200).json({
