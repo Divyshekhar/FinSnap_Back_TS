8beddd1da0052a0e280051817534078c6b81dd47
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-03-20 23:08:46 +0530

    fixing cors url

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors({
-    // origin: "https://finsnap.vercel.app"
-    origin: "http://localhost:3000"
+    origin: "https://finsnap.vercel.app"
+    // origin: "http://localhost:3000"
 }
 ));
 
